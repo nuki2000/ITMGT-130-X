c95f87fc2aae7d740f1ad027f037f6e09d55c793
nuki2000 <99396832+nuki2000@users.noreply.github.com>
2022-05-20 17:05:27 +0800

    Add files via upload

---
+++ assignments/assignment0.py
@@ -0,0 +1,36 @@
+
+'''Assignment 0
+This assignment is a giveaway assignment.
+By completing this assignment, you will familiarize yourself with this
+    course's submission mechanics.
+'''
+
+def three_number_average(x, y, z):
+    '''Item 1.
+    Three Number Average. 3 points.
+    Returns the average of three numbers.
+    Parameters
+    ----------
+    x: int
+        the first number
+    y: int
+        the second number
+    z: int
+        the third number
+    Returns
+    -------
+    float
+        the average of x, y, and z
+    '''
+    # Write your code below this line
+
+
+
+def three_number_average(x,y,z):
+
+    avg = (x + y + z) / 3
+    return avg
+
+example_average = three_number_average(1,2,3)
+
+print(example_average)
